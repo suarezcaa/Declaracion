70e18d6d11cd35af1a8698ec386e3dc1e8c5d6be
suarezcaa <122134383+suarezcaa@users.noreply.github.com>
2023-01-06 20:37:11 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const yesBtn = document.querySelector('#yesBtn');
 
 yesBtn.addEventListener('click',function () {
-    alert('Sabia que ibas a decir que si uwu')
+    alert('Te amo mucho. Sabia que no dudarias')
 });
 
 const noBtn = document.querySelector('#noBtn');
